2f6ab4877519a59201f624f86b1aca222a6e4438
davidAlgis <david.algis@tutamail.com>
2023-03-09 17:50:05 +0100

    ✨♻️ Add support to vertex buffer in DX11, reformat vertex_buffer_D3D11.cpp

--- Plugin/PluginInteropUnityCUDA/src/Buffer/vertex_buffer_D3D11.cpp
+++ Plugin/PluginInteropUnityCUDA/src/Buffer/vertex_buffer_D3D11.cpp
@@ -1,20 +1,18 @@
 #pragma once
 #include "vertex_buffer_D3D11.h"
 
-
-
 #if SUPPORT_D3D11
 
-VertexBuffer_D3D11::VertexBuffer_D3D11(void* bufferHandle, int size)
-	: VertexBuffer(bufferHandle, size)
+VertexBuffer_D3D11::VertexBuffer_D3D11(void *bufferHandle, int size)
+    : VertexBuffer(bufferHandle, size)
 {
-	Log::log().debugLogError("Vertex buffer interoperability on DX11 is not yet supported");
+
 }
 
 VertexBuffer_D3D11::~VertexBuffer_D3D11()
 {
-	// final check to be sure there was no mistake
-	CUDA_CHECK(cudaGetLastError());
+    // final check to be sure there was no mistake
+    CUDA_CHECK(cudaGetLastError());
 };
 
 /// <summary>
@@ -23,16 +21,17 @@ VertexBuffer_D3D11::~VertexBuffer_D3D11()
 void VertexBuffer_D3D11::registerBufferInCUDA()
 {
 
-	Log::log().debugLogError("Register buffer on DX11 is not yet supported");
-	// register the texture to cuda : it initialize the _pGraphicsResource
-	// CUDA_CHECK(cudaGraphicsD3D11RegisterResource(&_pGraphicsResource, (ID3D11Resource*)_bufferHandle, cudaGraphicsRegisterFlagsWriteDiscard));
+    ID3D11Buffer *bufferUnityDX11 = (ID3D11Buffer *)_bufferHandle;
+    // Log::log().debugLogError("Register buffer on DX11 is not yet supported");
+    // register the texture to cuda : it initialize the _pGraphicsResource
+    CUDA_CHECK(cudaGraphicsD3D11RegisterResource(
+        &_pGraphicsResource, (ID3D11Resource *)bufferUnityDX11,
+        cudaGraphicsRegisterFlagsNone));
 }
 
 void VertexBuffer_D3D11::unRegisterBufferInCUDA()
 {
-	Log::log().debugLogError("Unregister buffer on DX11 is not yet supported");
-	// CUDA_CHECK(cudaGraphicsUnregisterResource(_pGraphicsResource));
+    CUDA_CHECK(cudaGraphicsUnregisterResource(_pGraphicsResource));
 }
-
 
 #endif // #if SUPPORT_D3D11
