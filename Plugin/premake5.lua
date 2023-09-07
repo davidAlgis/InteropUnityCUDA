bc9f9164b25ed8a9b339182f4f1cebbed819c4e1
davidAlgis <david.algis@tutamail.com>
2023-09-07 11:31:30 +0200

    ⬆️ Update CUDA to 12.2

--- Plugin/premake5.lua
+++ Plugin/premake5.lua
@@ -2,14 +2,14 @@ function loadrequire(module, linkToRepository)
     local function requiref(module)
         require(module)
     end
-    res = pcall(requiref,module)
-    if not(res) then
+
+    res = pcall(requiref, module)
+    if not (res) then
         print("Could not find module :"
-        , module," it's available at this link : "
-        , linkToRepository)
+            , module, " it's available at this link : "
+            , linkToRepository)
     end
 end
-
 
 local root = "./"
 local pathTarget = "../InteropUnityCUDA/Assets/Plugin"
@@ -31,251 +31,256 @@ loadrequire('export-compile-commands', "https://github.com/null-black/premake-ex
 ---------------------------------
 -- [ WORKSPACE CONFIGURATION   --
 ---------------------------------
--- Setting up the workspace. A workspace can have multiple Projects. 
+-- Setting up the workspace. A workspace can have multiple Projects.
 -- In visual studio a workspace corresponds to a solution.
 workspace "PluginInteropUnity"
 
-    configurations { "Debug", "Release" } -- Optimization/General config mode in VS
-    platforms      { "x64"}            -- Dropdown platforms section in VS
-    -------------------------------
-    -- [ COMPILER/LINKER CONFIG] --
-    -------------------------------
-    -- Here we are setting up what differentiates the configurations that we called "Debug" and "Release"
-    filter "configurations:Debug"    defines { "DEBUG" }  symbols  "On"
-    filter "configurations:Release"  defines { "NDEBUG" } optimize "On"
-    filter { "platforms:*64" } architecture "x64"
+configurations { "Debug", "Release" } -- Optimization/General config mode in VS
+platforms { "x64" } -- Dropdown platforms section in VS
+-------------------------------
+-- [ COMPILER/LINKER CONFIG] --
+-------------------------------
+-- Here we are setting up what differentiates the configurations that we called "Debug" and "Release"
+filter "configurations:Debug"
+defines { "DEBUG" }
+symbols "On"
+filter "configurations:Release"
+defines { "NDEBUG" }
+optimize "On"
+filter { "platforms:*64" }
+architecture "x64"
 
-    defines {"PLUGINGLINTEROPCUDA_SHARED_EXPORTS"} symbols  "On"
+defines { "PLUGINGLINTEROPCUDA_SHARED_EXPORTS" }
+symbols "On"
 
-    -- building makefiles
-    filter { "action:gmake" }
+-- building makefiles
+filter { "action:gmake" }
 
-    -- building make files on mac specifically
-    filter { "system:macosx", "action:gmake"}
-        toolset "clang"
-        -- Toolset changes the compiler/linker/etc of the current project configuration.
-        --     "clang" is used in this case, because premake defaults to using gcc/g++ 
-        --     when used with the 'gmake' action, but macosx ships with clang by default
-        --     and if we don't set it explicitly we will get some warnings.
+-- building make files on mac specifically
+filter { "system:macosx", "action:gmake" }
+toolset "clang"
+-- Toolset changes the compiler/linker/etc of the current project configuration.
+--     "clang" is used in this case, because premake defaults to using gcc/g++
+--     when used with the 'gmake' action, but macosx ships with clang by default
+--     and if we don't set it explicitly we will get some warnings.
 
-    filter {}  -- clear filter when you know you no longer need it!
+filter {} -- clear filter when you know you no longer need it!
 
 -------------------------------
 -- [ PluginGLInteropCUDA ] --
-------------------------------- 
+-------------------------------
 project "PluginInteropUnityCUDA"
-    local rootProject = pathPluginInterop
-    location (rootProject)
-    language "C++"
-    targetdir (pathTarget, "SampleBasic")
-    objdir (rootProject .. "/temp/")
-    kind "SharedLib" 
-    
-    local SourceDir = rootProject .. "/src/";
-    local IncludeSubDir = pathPluginInteropIncludeSubdir;
-    local IncludeDir = pathPluginInteropInclude;
+local rootProject = pathPluginInterop
+location(rootProject)
+language "C++"
+targetdir(pathTarget, "SampleBasic")
+objdir(rootProject .. "/temp/")
+kind "SharedLib"
 
-    dependson {"Utilities"}
-    files
-    {
-        SourceDir .. "**.c",
-        pathSourceThirdPartyGLDir .. "**.c",
-        pathThirdPartyGLDir .. "**.h",
-        SourceDir .. "**.cpp",
-        IncludeDir .. "**.h", 
-        IncludeDir .. "**.hpp",
-        IncludeSubDir .. "**.h",
-        IncludeSubDir .. "**.hpp"
-    }
-    
-    includedirs
-    {
-        IncludeDir,
-        IncludeSubDir,
-        pathThirdPartyGLDir,
-        pathThirdPartyUnity,
-        pathUtilitiesInclude,
-    }
+local SourceDir = rootProject .. "/src/";
+local IncludeSubDir = pathPluginInteropIncludeSubdir;
+local IncludeDir = pathPluginInteropInclude;
 
+dependson { "Utilities" }
+files
+{
+    SourceDir .. "**.c",
+    pathSourceThirdPartyGLDir .. "**.c",
+    pathThirdPartyGLDir .. "**.h",
+    SourceDir .. "**.cpp",
+    IncludeDir .. "**.h",
+    IncludeDir .. "**.hpp",
+    IncludeSubDir .. "**.h",
+    IncludeSubDir .. "**.hpp"
+}
 
-    libdirs
-    {
-        pathTarget
-    }
-
-    filter { "system:windows" }
-        links{nameUtilitiesLib, "OpenGL32"}
-
-    filter { "system:not windows" }
-        links { "GL" , nameUtilitiesLib}
-
-    filter {}
+includedirs
+{
+    IncludeDir,
+    IncludeSubDir,
+    pathThirdPartyGLDir,
+    pathThirdPartyUnity,
+    pathUtilitiesInclude,
+}
 
 
-    -- Add necessary build customization using standard Premake5
-    -- This assumes you have installed Visual Studio integration for CUDA
-    -- Here we have it set to 11.4
-    buildcustomizations "BuildCustomizations/CUDA 12.1"
-    -- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
+libdirs
+{
+    pathTarget
+}
 
-    -- CUDA specific properties
-    cudaFiles {SourceDir .. "**.cu"}
-    cudaMaxRegCount "32"
+filter { "system:windows" }
+links { nameUtilitiesLib, "OpenGL32" }
 
-    -- Let's compile for all supported architectures (and also in parallel with -t0)
-    cudaCompilerOptions {"-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
-                         "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
-                         "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
-                         "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0"}                      
+filter { "system:not windows" }
+links { "GL", nameUtilitiesLib }
 
-    -- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
-    if os.target() == "linux" then 
-        linkoptions {"-L/usr/local/cuda/lib64 -lcudart"}
-    end
+filter {}
 
-    filter "configurations:release"
-    cudaFastMath "On" -- enable fast math for release
-    filter ""
+
+-- Add necessary build customization using standard Premake5
+-- This assumes you have installed Visual Studio integration for CUDA
+-- Here we have it set to 11.4
+buildcustomizations "BuildCustomizations/CUDA 12.2"
+-- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
+
+-- CUDA specific properties
+cudaFiles { SourceDir .. "**.cu" }
+cudaMaxRegCount "32"
+
+-- Let's compile for all supported architectures (and also in parallel with -t0)
+cudaCompilerOptions { "-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
+    "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
+    "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
+    "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0" }
+
+-- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
+if os.target() == "linux" then
+    linkoptions { "-L/usr/local/cuda/lib64 -lcudart" }
+end
+
+filter "configurations:release"
+cudaFastMath "On" -- enable fast math for release
+filter ""
 
 
 
 
 -------------------------------
 -- [ SampleBasic ] --
-------------------------------- 
+-------------------------------
 project "SampleBasic"
-    local locationProject = pathSampleProject
-    local rootProject = root .. locationProject
-    location (rootProject)
-    language "C++"
-    targetdir (pathTarget)
-    objdir (rootProject .. "/temp/")
-    kind "SharedLib" 
-    
-    local SourceDir = rootProject .. "/src/";
-    local IncludeSubDir = rootProject .. "/include/**";
-    local IncludeDir = rootProject .. "/include/";
-    dependson{"PluginInteropUnityCUDA"}
-    files
-    {
-        SourceDir .. "**.c",
-        -- SourceThirdPartyGLDir .. "**.c",
-        -- ThirdPartyGLDir .. "**.h",
-        SourceDir .. "**.cpp",
-        IncludeDir .. "**.h", 
-        IncludeDir .. "**.cuh", 
-        IncludeDir .. "**.hpp",
-        IncludeSubDir .. "**.h",
-        IncludeSubDir .. "**.cuh",
-        IncludeSubDir .. "**.hpp"
-    }
-    
-    includedirs
-    {
-        IncludeDir,
-        IncludeSubDir,
-        pathThirdPartyGLDir,
-        pathUtilitiesInclude,
-        pathPluginInteropInclude,
-        pathPluginInteropIncludeSubdir,
-        pathThirdPartyUnity
-    }
+local locationProject = pathSampleProject
+local rootProject = root .. locationProject
+location(rootProject)
+language "C++"
+targetdir(pathTarget)
+objdir(rootProject .. "/temp/")
+kind "SharedLib"
+
+local SourceDir = rootProject .. "/src/";
+local IncludeSubDir = rootProject .. "/include/**";
+local IncludeDir = rootProject .. "/include/";
+dependson { "PluginInteropUnityCUDA" }
+files
+{
+    SourceDir .. "**.c",
+    -- SourceThirdPartyGLDir .. "**.c",
+    -- ThirdPartyGLDir .. "**.h",
+    SourceDir .. "**.cpp",
+    IncludeDir .. "**.h",
+    IncludeDir .. "**.cuh",
+    IncludeDir .. "**.hpp",
+    IncludeSubDir .. "**.h",
+    IncludeSubDir .. "**.cuh",
+    IncludeSubDir .. "**.hpp"
+}
+
+includedirs
+{
+    IncludeDir,
+    IncludeSubDir,
+    pathThirdPartyGLDir,
+    pathUtilitiesInclude,
+    pathPluginInteropInclude,
+    pathPluginInteropIncludeSubdir,
+    pathThirdPartyUnity
+}
 
 
-    libdirs
-    {
-        pathTarget
-    }
+libdirs
+{
+    pathTarget
+}
 
-    --here we need the dll INTEROP too
-    filter { "system:windows" }
-        links{nameUtilitiesLib, "OpenGL32", namePluginInteropLib}
+--here we need the dll INTEROP too
+filter { "system:windows" }
+links { nameUtilitiesLib, "OpenGL32", namePluginInteropLib }
 
-    filter { "system:not windows" }
-        links { "GL" , nameUtilitiesLib}
+filter { "system:not windows" }
+links { "GL", nameUtilitiesLib }
 
-    filter {}
+filter {}
 
 
-    -- Add necessary build customization using standard Premake5
-    -- This assumes you have installed Visual Studio integration for CUDA
-    -- Here we have it set to 11.4
-    buildcustomizations "BuildCustomizations/CUDA 12.1"
-    -- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
+-- Add necessary build customization using standard Premake5
+-- This assumes you have installed Visual Studio integration for CUDA
+-- Here we have it set to 12.2
+buildcustomizations "BuildCustomizations/CUDA 12.2"
+-- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
 
-    -- CUDA specific properties
-    cudaFiles {SourceDir .. "**.cu"}
-    cudaMaxRegCount "32"
+-- CUDA specific properties
+cudaFiles { SourceDir .. "**.cu" }
+cudaMaxRegCount "32"
 
-    -- Let's compile for all supported architectures (and also in parallel with -t0)
-    cudaCompilerOptions {"-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
-                         "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
-                         "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
-                         "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0"}                      
+-- Let's compile for all supported architectures (and also in parallel with -t0)
+cudaCompilerOptions { "-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
+    "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
+    "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
+    "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0" }
 
-    -- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
-    if os.target() == "linux" then 
-        linkoptions {"-L/usr/local/cuda/lib64 -lcudart"}
-    end
+-- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
+if os.target() == "linux" then
+    linkoptions { "-L/usr/local/cuda/lib64 -lcudart" }
+end
 
-    filter "configurations:release"
-    cudaFastMath "On" -- enable fast math for release
-    filter ""
+filter "configurations:release"
+cudaFastMath "On" -- enable fast math for release
+filter ""
 
 
 -------------------------------
 -- [ Utilities ] --
-------------------------------- 
+-------------------------------
 project "Utilities"
-    local rootProject = pathUtilities
-    location (rootProject)
-    kind "SharedLib" 
-    language "C++"
-    targetdir (pathTarget)
-    objdir (rootProject .. "/temp/".. locationProject)
-    
-    local SourceDir = rootProject .. "/src/";
-    local IncludeDir = pathUtilitiesInclude;
-    
-    -- what files the visual studio project/makefile/etc should know about
-    files
-    {
-        SourceDir .. "**.c",
-        SourceDir .. "**.cpp",
-        IncludeDir .. "**.h", 
-        IncludeDir .. "**.hpp",
-    }
+local rootProject = pathUtilities
+location(rootProject)
+kind "SharedLib"
+language "C++"
+targetdir(pathTarget)
+objdir(rootProject .. "/temp/" .. locationProject)
 
-    includedirs
-    {
-        IncludeDir
-    }
+local SourceDir = rootProject .. "/src/";
+local IncludeDir = pathUtilitiesInclude;
 
-    flags { "NoImportLib" }
-    flags {}
+-- what files the visual studio project/makefile/etc should know about
+files
+{
+    SourceDir .. "**.c",
+    SourceDir .. "**.cpp",
+    IncludeDir .. "**.h",
+    IncludeDir .. "**.hpp",
+}
 
-    defines {"UTILITIES_SHARED", "UTILITIES_SHARED_EXPORTS"} symbols  "On"
+includedirs
+{
+    IncludeDir
+}
 
-    -- Add necessary build customization using standard Premake5
-    -- This assumes you have installed Visual Studio integration for CUDA
-    -- Here we have it set to 11.4
-    buildcustomizations "BuildCustomizations/CUDA 12.1"
-    -- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
+flags { "NoImportLib" }
+flags {}
 
-    cudaMaxRegCount "32"
-    -- Let's compile for all supported architectures (and also in parallel with -t0)
-    cudaCompilerOptions {"-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
-                         "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
-                         "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
-                         "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0"}                      
+defines { "UTILITIES_SHARED", "UTILITIES_SHARED_EXPORTS" }
+symbols "On"
 
-    -- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
-    if os.target() == "linux" then 
-        linkoptions {"-L/usr/local/cuda/lib64 -lcudart"}
-    end
+-- Add necessary build customization using standard Premake5
+-- This assumes you have installed Visual Studio integration for CUDA
+-- Here we have it set to 11.4
+buildcustomizations "BuildCustomizations/CUDA 12.2"
+-- cudaPath "/usr/local/cuda" -- Only affects linux, because the windows builds get CUDA from the VS extension
 
-    filter "configurations:release"
-    cudaFastMath "On" -- enable fast math for release
-    filter ""
+cudaMaxRegCount "32"
+-- Let's compile for all supported architectures (and also in parallel with -t0)
+cudaCompilerOptions { "-arch=sm_52", "-gencode=arch=compute_52,code=sm_52", "-gencode=arch=compute_60,code=sm_60",
+    "-gencode=arch=compute_61,code=sm_61", "-gencode=arch=compute_70,code=sm_70",
+    "-gencode=arch=compute_75,code=sm_75", "-gencode=arch=compute_80,code=sm_80",
+    "-gencode=arch=compute_86,code=sm_86", "-gencode=arch=compute_86,code=compute_86", "-t0" }
 
+-- On Windows, the link to cudart is done by the CUDA extension, but on Linux, this must be done manually
+if os.target() == "linux" then
+    linkoptions { "-L/usr/local/cuda/lib64 -lcudart" }
+end
 
+filter "configurations:release"
+cudaFastMath "On" -- enable fast math for release
+filter ""
