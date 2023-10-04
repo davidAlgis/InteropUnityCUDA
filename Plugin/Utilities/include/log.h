c99df819af2bbbc887420f5caa83512e89d775c9
davidAlgis <david.algis@tutamail.com>
2023-10-04 18:09:28 +0200

    🐛♻️ Fixed lambda function for print and reformat file

--- Plugin/Utilities/include/log.h
+++ Plugin/Utilities/include/log.h
@@ -1,16 +1,23 @@
 #pragma once
-#include "framework.h" 
-#include <fstream>  
-#include <vector>
+#include "framework.h"
+#include <fstream>
 #include <functional>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 
 extern "C"
 {
 
-    #define GRUMBLE(code, msg) {if(code != 0){Log::log().debugLogError(msg); return code;}} 
-    #define SUCCESS_INTEROP_CODE 0
+#define GRUMBLE(code, msg)                                                     \
+    {                                                                          \
+        if ((code) != 0)                                                       \
+        {                                                                      \
+            Log::log().debugLogError(msg);                                     \
+            return code;                                                       \
+        }                                                                      \
+    }
+#define SUCCESS_INTEROP_CODE 0
 
     enum class logType
     {
@@ -20,93 +27,91 @@ extern "C"
     };
 
     /// <summary>
-    /// External function for dll, it will merge all log and separate them by '\n'
+    /// External function for dll, it will merge all log and separate them by
+    /// '\n'
     /// </summary>
-    /// <param name="data">a pointer to a string that will contains a 
-    /// merge of all log and separate them by '\n'. We use a reference to 
+    /// <param name="data">a pointer to a string that will contains a
+    /// merge of all log and separate them by '\n'. We use a reference to
     /// a const char* to permit marshalling</param>
     /// <param name="type">Type of log to get</param>
-    void UTILITIES_API GetLastLog(const char*& data, logType type);
+    void UTILITIES_API GetLastLog(const char *&data, logType type);
 
     class Log
     {
-    private:
-
+        private:
         std::fstream _logFile;
-        
+
         const size_t MAX_LOG_SIZE = 50;
         std::vector<std::string> _logInfoVector;
         std::vector<std::string> _logWarningVector;
         std::vector<std::string> _logErrorVector;
         std::string _logBufferSum;
-        std::function<void(std::string, std::fstream&)> _infoPrintFunction;
-        std::function<void(std::string, std::fstream&)> _warningPrintFunction;
-        std::function<void(std::string, std::fstream&)> _errorPrintFunction;
+        std::function<void(std::string, std::fstream &)> _infoPrintFunction;
+        std::function<void(std::string, std::fstream &)> _warningPrintFunction;
+        std::function<void(std::string, std::fstream &)> _errorPrintFunction;
 
-    public:
+        public:
         // SINGLETON
-        UTILITIES_API static Log& log();
+        UTILITIES_API static Log &log();
 
-        UTILITIES_API Log(const Log&) = delete;
-        UTILITIES_API Log& operator=(const Log&) = delete;
-        UTILITIES_API Log(Log&&) = delete;
-        UTILITIES_API Log& operator=(Log&&) = delete;
-
+        Log(const Log &) = delete;
+        Log &operator=(const Log &) = delete;
+        Log(Log &&) = delete;
+        Log &operator=(Log &&) = delete;
 
         UTILITIES_API Log();
         UTILITIES_API ~Log();
 
         /// <summary>
-        /// This function is called when debulLog is called, it will write the string in parameters 
-        /// and the fstream is the log file 
+        /// This function is called when debulLog is called, it will write the
+        /// string in parameters and the fstream is the log file
         /// </summary>
-        UTILITIES_API void setInfoPrintFunction(std::function<void(std::string, std::fstream&)> printFunc);
-
+        UTILITIES_API void setInfoPrintFunction(
+            std::function<void(std::string, std::fstream &)> printFunc);
 
         /// <summary>
-        /// This function is called when debulLogWarning is called, it will write the string in parameters 
-        /// and the fstream is the log file 
+        /// This function is called when debulLogWarning is called, it will
+        /// write the string in parameters and the fstream is the log file
         /// </summary>
-        UTILITIES_API void setWarningPrintFunction(std::function<void(std::string, std::fstream&)> printFunc);
-
-
-        /// <summary>
-        /// This function is called when debulLogError is called, it will write the string in parameters 
-        /// and the fstream is the log file 
-        /// </summary>
-        UTILITIES_API void setErrorPrintFunction(std::function<void(std::string, std::fstream&)> printFunc);
+        UTILITIES_API void setWarningPrintFunction(
+            std::function<void(std::string, std::fstream &)> printFunc);
 
         /// <summary>
-        /// It will merge all log and separate them by '\n' 
+        /// This function is called when debulLogError is called, it will write
+        /// the string in parameters and the fstream is the log file
         /// </summary>
-        /// <param name="data">a pointer to a string that will contains a 
-        /// merge of all log and separate them by '\n'. We use a reference to 
+        UTILITIES_API void setErrorPrintFunction(
+            std::function<void(std::string, std::fstream &)> printFunc);
+
+        /// <summary>
+        /// It will merge all log and separate them by '\n'
+        /// </summary>
+        /// <param name="data">a pointer to a string that will contains a
+        /// merge of all log and separate them by '\n'. We use a reference to
         /// a const char* to permit marshalling</param>
         /// <param name="type">Type of log to get</param>
-        UTILITIES_API void getMergedLog(const char*& data, logType type);
+        UTILITIES_API void getMergedLog(const char *&data, logType type);
 
         /// <summary>
         /// Call this function to write an info in log
         /// </summary>
         /// <param name="text">info to write in log</param>
-        UTILITIES_API void debugLog(const std::string text);
+        UTILITIES_API void debugLog(std::string text);
 
         /// <summary>
         /// Call this function to write an warning in log
         /// This will add Warning : before message
         /// </summary>
         /// <param name="text">warning to write in log</param>
-        UTILITIES_API void debugLogWarning(const std::string text);
-
+        UTILITIES_API void debugLogWarning(std::string text);
 
         /// <summary>
         /// Call this function to write an error in log
         /// This will add Error : before message
         /// </summary>
         /// <param name="text">error to write in log</param>
-        UTILITIES_API void debugLogError(const std::string text);
+        UTILITIES_API void debugLogError(std::string text);
 
-        UTILITIES_API void extractLog(std::vector<std::string>& logVector);
+        UTILITIES_API void extractLog(std::vector<std::string> &logVector);
     };
-
 }
--- Plugin/Utilities/src/log.cpp
+++ Plugin/Utilities/src/log.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 #include <ctime>
+#include <utility>
 
 #if !defined PROJECT_NAME
 #define PROJECT_NAME ""
@@ -8,14 +9,17 @@
 extern "C"
 {
 
-    void GetLastLog(const char*& data, logType type) { Log::log().getMergedLog(data, type); }
+    void GetLastLog(const char *&data, logType type)
+    {
+        Log::log().getMergedLog(data, type);
+    }
 
-    Log& Log::log()
+    Log &Log::log()
     {
         // Static are guarantee to be created only once and is thread safe
         // To be sure this is the only instance even across multiple processes
         // the declaration / initialisation must be declared in a compiled unit
-        // ! This function should not be inlined !
+        // ! This function should not be inline !
         static Log sInstance{};
         return sInstance;
     }
@@ -33,25 +37,31 @@ extern "C"
         localtime_s(&now_tm, &now);
         asctime_s(str, 26, &now_tm);
 
-        _logFile << "Log file - Project : " << projectName << " - Begin at : " << str << std::endl;
+        _logFile << "Log file - Project : " << projectName
+                 << " - Begin at : " << str << std::endl;
 
-        for (int i = 0; i < 128; i++) _logFile << "-";
+        for (int i = 0; i < 128; i++)
+        {
+            _logFile << "-";
+        }
         _logFile << std::endl;
 
         _logInfoVector.reserve(MAX_LOG_SIZE);
         _logWarningVector.reserve(MAX_LOG_SIZE);
         _logErrorVector.reserve(MAX_LOG_SIZE);
         _logBufferSum.reserve(128);
-        _infoPrintFunction = [](const std::string& msg, std::fstream& logFile) {
+        _infoPrintFunction = [](const std::string &msg, std::fstream &logFile) {
             logFile << msg << std::endl;
             std::cout << msg << std::endl;
         };
-        _warningPrintFunction = [](const std::string& msg, std::fstream& logFile) {
+        _warningPrintFunction = [](const std::string &msg,
+                                   std::fstream &logFile) {
             logFile << msg << std::endl;
             std::cout << msg << std::endl;
         };
 
-        _errorPrintFunction = [](const std::string& msg, std::fstream& logFile) {
+        _errorPrintFunction = [](const std::string &msg,
+                                 std::fstream &logFile) {
             logFile << msg << std::endl;
             std::cout << msg << std::endl;
         };
@@ -61,65 +71,81 @@ extern "C"
     {
         std::string projectName = PROJECT_NAME;
 
-        for (int i = 0; i < 128; i++) _logFile << "-";
+        for (int i = 0; i < 128; i++)
+        {
+            _logFile << "-";
+        }
         _logFile << std::endl;
 
         // get time now
-        time_t now = time(NULL);
+        time_t now = time(nullptr);
         tm now_tm = {};
         char str[26] = {};
         localtime_s(&now_tm, &now);
         asctime_s(str, 26, &now_tm);
 
-        _logFile << "End Log file - Project : " << projectName << " - End at : " << str
-                 << std::endl;
+        _logFile << "End Log file - Project : " << projectName
+                 << " - End at : " << str << std::endl;
         _logFile.close();
     }
 
-    void Log::setInfoPrintFunction(std::function<void(std::string, std::fstream&)> printFunc)
+    void Log::setInfoPrintFunction(
+        std::function<void(std::string, std::fstream &)> printFunc)
     {
-        _infoPrintFunction = printFunc;
+        _infoPrintFunction = std::move(printFunc);
     }
 
-    void Log::setWarningPrintFunction(std::function<void(std::string, std::fstream&)> printFunc)
+    void Log::setWarningPrintFunction(
+        std::function<void(std::string, std::fstream &)> printFunc)
     {
-        _warningPrintFunction = printFunc;
+        _warningPrintFunction = std::move(printFunc);
     }
 
-    void Log::setErrorPrintFunction(std::function<void(std::string, std::fstream&)> printFunc)
+    void Log::setErrorPrintFunction(
+        std::function<void(std::string, std::fstream &)> printFunc)
     {
-        _errorPrintFunction = printFunc;
+        _errorPrintFunction = std::move(printFunc);
     }
 
-    void Log::debugLog(const std::string text)
+    void Log::debugLog(std::string text)
     {
+        std::string msg = "Info " + text;
         _infoPrintFunction(text, _logFile);
 
-        if (_logInfoVector.size() >= MAX_LOG_SIZE) _logInfoVector.pop_back();
+        if (_logInfoVector.size() >= MAX_LOG_SIZE)
+        {
+            _logInfoVector.pop_back();
+        }
 
         _logInfoVector.push_back(text);
     }
 
-    void Log::debugLogWarning(const std::string text)
+    void Log::debugLogWarning(std::string text)
     {
-        _warningPrintFunction("Warning " + text, _logFile);
-        // _logFile << "Warning : " << text << std::endl;
+        std::string msg = "Warning " + text;
+        _warningPrintFunction(msg, _logFile);
 
-        if (_logWarningVector.size() >= MAX_LOG_SIZE) _logWarningVector.pop_back();
+        if (_logWarningVector.size() >= MAX_LOG_SIZE)
+        {
+            _logWarningVector.pop_back();
+        }
 
         _logWarningVector.push_back(text);
     }
 
-    void Log::debugLogError(const std::string text)
+    void Log::debugLogError(std::string text)
     {
-        _errorPrintFunction("Error " + text, _logFile);
-        // _logFile << "Error : " << text << std::endl;
-        if (_logErrorVector.size() >= MAX_LOG_SIZE) _logErrorVector.pop_back();
+        std::string msg = "Error " + text;
+        _errorPrintFunction(msg, _logFile);
+        if (_logErrorVector.size() >= MAX_LOG_SIZE)
+        {
+            _logErrorVector.pop_back();
+        }
 
         _logErrorVector.push_back(text);
     }
 
-    void Log::getMergedLog(const char*& data, logType logType)
+    void Log::getMergedLog(const char *&data, logType logType)
     {
         switch (logType)
         {
@@ -135,17 +161,22 @@ extern "C"
             extractLog(_logErrorVector);
             break;
         }
-        default: debugLogError("Unknown log type"); return;
+        default:
+            debugLogError("Unknown log type");
+            return;
         }
 
         data = _logBufferSum.c_str();
     }
 
-    void Log::extractLog(std::vector<std::string>& logVector)
+    void Log::extractLog(std::vector<std::string> &logVector)
     {
         _logBufferSum.clear();
         _logBufferSum.reserve(128 * logVector.size());
-        for (auto&& str : logVector) { _logBufferSum += str + "\n"; }
+        for (auto &&str : logVector)
+        {
+            _logBufferSum += str + "\n";
+        }
 
         // we'll clear log
         logVector.clear();
