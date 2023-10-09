b4fd7ae0120b22fb13c6be353334f320afb03a06
davidAlgis <david.algis@tutamail.com>
2023-10-09 16:58:03 +0200

    ♻️ Refactor Action.h

--- Plugin/PluginInteropUnityCUDA/include/Action.h
+++ Plugin/PluginInteropUnityCUDA/include/Action.h
@@ -2,23 +2,30 @@
 #include "IUnityGraphics.h"
 #include <functional>
 
-/// <summary>
-/// Base class to derive from if you want to execute some function on graphics object
-/// </summary>
+/**
+ * @class      Action
+ *
+ * @brief      Base class to derive from if you want to execute some function on
+ * graphics object
+ *
+ *
+ * @example action_sample_texture_array.cpp shows an example of use of this
+ * class with a texture array.
+ */
 class UNITY_INTERFACE_EXPORT Action
 {
-public:
-	Action() = default;
-	~Action() = default;
-	Action(const Action&) = default;
-	Action(Action&&) = default;
-	Action& operator=(const Action&) = default;
-	Action& operator=(Action&&) = default;
+    public:
+    Action() = default;
+    ~Action() = default;
+    Action(const Action &) = default;
+    Action(Action &&) = default;
+    Action &operator=(const Action &) = default;
+    Action &operator=(Action &&) = default;
 
-	// The name of the function are just here to make indication about their first purposes 
-	// but you can use them in another purposes.
-	virtual int Start() = 0;
-	virtual int Update() = 0;
-	virtual int OnDestroy() = 0;
-	bool IsActive = true;
+    // The name of the function are just here to make indication about their
+    // first purposes but you can use them in another purposes.
+    virtual int Start() = 0;
+    virtual int Update() = 0;
+    virtual int OnDestroy() = 0;
+    bool IsActive = true;
 };
