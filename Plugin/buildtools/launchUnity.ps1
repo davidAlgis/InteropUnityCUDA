f8347e69050a272cefc848d27fbb6c2f531898f0
davidAlgis <david.algis@tutamail.com>
2024-02-19 15:10:51 +0100

    🔨 Copy build to unity at unity launch

--- Plugin/buildtools/launchUnity.ps1
+++ Plugin/buildtools/launchUnity.ps1
@@ -1,7 +1,9 @@
 . $PSScriptRoot\dependencies.ps1
+. $PSScriptRoot\copyBuildToUnity.ps1
 
 $projectPath = "$PSScriptRoot\..\..\InteropUnityCUDA"
-
 Write-Host "Launch Unity interop project at $projectPath..."
+
+
 & $Env:UNITY_BIN -projectPath "$projectPath"
 
